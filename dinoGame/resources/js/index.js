f8baaa48ee2a71b99aee6ba2958f8dd147d4ea1a
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-12-26 16:48:39 -0300

    feat: adicionado animação do cactu

--- dinoGame/resources/js/index.js
+++ dinoGame/resources/js/index.js
@@ -8,11 +8,18 @@ function jump(){
         setTimeout(()=> {
             dino.classList.remove("jump")
         }, 300)
-    }
-
-    
+    } 
 }
+
+let isAlive = setInterval(function(){
+    
+
+    let dinoTop = window.getComputedStyle(dino).getPropertyValue("top")
+    console.log("check")
+    
+}, 10)
 
 document.addEventListener("keydown", (event) => {
     jump()
 })
+
