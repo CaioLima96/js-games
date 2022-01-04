ec7496e5ee6ca08ff176c8b9025b78d756872ce3
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-01-04 12:09:50 -0300

    FEAT: Adicionado reset do score quando inicia e reseta o jogo do DINOGAME1

--- dinoGame/resources/js/startReset.js
+++ dinoGame/resources/js/startReset.js
@@ -3,9 +3,13 @@ function start(){
         cactus.classList.add("moveLeft")
         document.getElementById("startBtn").style.display = "none"
         document.getElementById("resetBtn").style.display = "none"
+        document.getElementById('score').innerHTML = `<p>Pontuação: ${finalScore = 0}</p>`
+        starImg.forEach(scoreReset)
     }
 }
 
 function reset(){
     document.getElementById("resetBtn").style.display = "flex"
+    document.getElementById('score').innerHTML = `<p>Pontuação: ${finalScore = 0}</p>`
+    starImg.forEach(scoreReset)
 }
