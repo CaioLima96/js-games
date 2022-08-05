8804772e4c7ebbd5503b85576586c29035bc8a5a
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-08-05 14:47:47 -0300

    chore: terminado o vídeo - memoryGame

--- memoryGame/resources/js/app.js
+++ memoryGame/resources/js/app.js
@@ -1,7 +1,7 @@
 //Grab a couple of things
 const section = document.querySelector('section')
 const playerLivesCount = document.querySelector('span')
-const playerLives = 6
+let playerLives = 6
 
 
 //Link text
@@ -78,19 +78,62 @@ const checkCards = (e) => {
     const clickedCard = e.target
     clickedCard.classList.add('flipped')
     const flippedCards = document.querySelectorAll('.flipped')
-    
+    const toggleCard = document.querySelectorAll('.toggleCard')
+
     console.log(clickedCard)
 
     //Logic
     if(flippedCards.length === 2) {
         
         if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
+            
             console.log('match')
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped')
+                card.style.pointerEvents = 'none'
+            })
+
         } else {
+
             console.log('wrong')
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped')
+                setTimeout(() => card.classList.remove('toggleCard'), 1000)
+            })
+            playerLives--
+            playerLivesCount.textContent = playerLives
+            if (playerLives === 0) restart("tente outra vez")
         }
-    } 
+    }
+
+    //Check if we won the game
+    if(toggleCard.length === 16) {
+        restart("vc venceu")
+    }
 }
 
+
+//Restart
+const restart = (text) => {
+    
+    let cardData = randomize()
+    let faces = document.querySelectorAll('.face')
+    let cards = document.querySelectorAll('.card')
+    section.style.pointerEvents = 'none'
+
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove('toggleCard')
+        //Randomize
+        setTimeout(() => {
+            cards[index].style.pointerEvents = 'all'
+            faces[index].src = item.img
+            cards[index].setAttribute('name', item.name)
+            section.style.pointerEvents = 'all'
+        }, 1000)
+    })
+    playerLives = 6
+    playerLivesCount.textContent = playerLives
+    setTimeout(() => window.alert(text), 100)
+}
 
 cardGenerator()
